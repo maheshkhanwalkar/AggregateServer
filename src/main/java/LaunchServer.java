e6939b7f958d190f433af429b8a3c8f7a15353c8
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2015-04-13 19:24:57 -0400

    Added LaunchServer Stub

---
+++ src/main/java/LaunchServer.java
@@ -0,0 +1,26 @@
+/*
+    Copyright 2015 Mahesh Khanwalkar
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/* Reads XML Configuration Options & Launches the AggregateServer
+ * on all machines in the cluster */
+
+public class LaunchServer
+{
+    public static void main(String[] args)
+    {
+        //TODO
+    }
+}
