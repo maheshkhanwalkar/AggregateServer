228a44202300f6a076ac2766c7d7d2a2dfb6ef20
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2015-04-15 22:03:37 -0400

    Moved Class

--- src/main/java/com/inixsoftware/haggregate/aggregateserver/socket/master/MasterHandler.java
+++ src/main/java/com/inixsoftware/haggregate/aggregateserver/socket/master/MasterHandler.java
@@ -16,10 +16,10 @@ package com.inixsoftware.haggregate.aggregateserver.socket.master;
     limitations under the License.
 */
 
-import com.inixsoftware.haggregate.aggregateserver.lang.CommunicationValidator;
+import com.inixsoftware.haggregate.aggregateserver.talk.ClientTalk;
 import org.apache.log4j.Logger;
 
-import java.io.*;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -40,104 +40,6 @@ public class MasterHandler implements Runnable
     public MasterHandler(int port)
     {
         this.port = port;
-    }
-
-    private class ClientTalk implements Runnable
-    {
-        private Socket client;
-        private BufferedReader br;
-
-        private BufferedWriter bw;
-        private String hostName;
-
-        public ClientTalk(Socket client)
-        {
-            this.client = client;
-            hostName = "[" + client.getInetAddress().getHostName() + "]";
-        }
-
-        private void cleanUp()
-        {
-            try
-            {
-                br.close();
-                bw.close();
-
-                client.close();
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-            }
-        }
-
-        private String hostName()
-        {
-            return hostName;
-        }
-
-        public void run()
-        {
-            logger.info("Processing Request");
-
-            try
-            {
-                br = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-
-                String api = br.readLine();
-                if(!CommunicationValidator.isSupportedAPI(api))
-                {
-                    logger.info(hostName() + " Unsupported API - Rejecting!");
-
-                    bw.write("ERR:BAD_API\n");
-                    bw.flush();
-
-                    cleanUp();
-                    logger.info(hostName() + " Connection Closed");
-
-                    return;
-                }
-
-                bw.write("ACK\n");
-                bw.flush();
-
-                while(true)
-                {
-                    String cmd = br.readLine();
-
-                    if(!CommunicationValidator.isValidCMD(cmd))
-                    {
-                        logger.info(hostName() + " Invalid Command - Rejecting!");
-
-                        bw.write("ERR:BAD_CMD\n");
-                        bw.flush();
-
-                        cleanUp();
-                        break;
-                    }
-
-                    if(cmd.equals("CMD:CLOSE_SOCKET"))
-                    {
-                        bw.write("ACK\n");
-                        bw.flush();
-
-                        cleanUp();
-                        break;
-                    }
-
-                    //TODO process command
-                }
-
-                logger.info(hostName() + " Connection Closed");
-
-            }
-            catch (IOException e)
-            {
-                logger.fatal(null, e);
-            }
-
-        }
     }
 
     public void run()
